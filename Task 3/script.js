8f0be8a6295cd08d4e9939f452c0f9f0c6e7039b
Gaidamavičius <marius.gaidamavicius@thermofisher.com>
2022-05-10 23:22:51 +0300

    3rd task completed

--- Task 3/script.js
+++ Task 3/script.js
@@ -12,3 +12,44 @@ turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
 const ENDPOINT = 'https://api.github.com/users';
+const SHOW_USERS_BUTTON = document.getElementById('btn');
+const MESSAGE_P_TAG = document.getElementById('message');
+const OUTPUT_TAG = document.getElementById('output');
+
+function fetchData() {
+    SHOW_USERS_BUTTON.addEventListener('click', () => {
+    MESSAGE_P_TAG.remove();
+    fetch(ENDPOINT)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                const WRAPPER = document.createElement('div');
+                WRAPPER.classList.add("div-wrapper");
+
+                const IMG_ELEMENT = createElement('img',element.avatar_url);
+                IMG_ELEMENT.classList.add('image');
+                WRAPPER.appendChild(IMG_ELEMENT);
+
+                const H2_ELEMENT = createElement('h2',element.login);       
+                WRAPPER.appendChild(H2_ELEMENT);
+
+                OUTPUT_TAG.appendChild(WRAPPER);
+            });
+        })
+    })
+};
+
+fetchData();
+
+function createElement(ele, text) {
+    const elem = document.createElement(ele);
+    if(ele === 'img') {
+        elem.src = text;
+    } else {
+        elem.innerHTML = text; 
+    }
+    return elem
+}
+
+
+
