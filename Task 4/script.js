135a9fc92df89c1ecfddf21adf736583751cd08f
Gaidamavičius <marius.gaidamavicius@thermofisher.com>
2022-05-11 14:05:45 +0300

    4th task completed

--- Task 4/script.js
+++ Task 4/script.js
@@ -9,4 +9,39 @@ Pastaba: Sukurta kortelė, kurioje yra informacija apie automobilį (brand), tur
 turėti bent minimalų stilių ir būti responsive;
 -------------------------------------------------------------------------- */
 
-const ENDPOINT = 'cars.json';
+const ENDPOINT = './cars.json';
+const OUTPUT_TAG = document.getElementById('output');
+
+function fetchData() {
+    fetch(ENDPOINT)
+        .then(response => response.json())
+        .then(data => {
+            data.forEach(element => {
+                const WRAPPER = document.createElement('div');
+                WRAPPER.classList.add("div-wrapper");
+
+                const BRAND_ELEMENT = createElement('div',element.brand);
+                WRAPPER.appendChild(BRAND_ELEMENT);
+
+                const LIST_ELEMENT = createElement('ol');       
+                WRAPPER.appendChild(LIST_ELEMENT);
+
+                    element.models.forEach(ele => {
+                        const LIST_ITEM = createElement('li',ele);
+                        BRAND_ELEMENT.appendChild(LIST_ITEM);
+                    });
+
+                OUTPUT_TAG.appendChild(WRAPPER);
+            });
+        })
+};
+
+fetchData();
+
+function createElement(ele, text) {
+    const elem = document.createElement(ele);
+          elem.innerHTML = text;
+          return elem
+}
+
+    
